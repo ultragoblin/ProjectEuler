e45f7898e1b4166156f20785daa775db0d9595cd
Ultragoblin <angry.person1@gmail.com>
2014-02-13 14:44:39 +0400

    p14

---
+++ p14.py
@@ -0,0 +1,28 @@
+__author__ = 'guardian'
+
+start = 1000000
+n = 0
+count = 0
+result = 0
+db = {}
+
+for start in range(1, 1000000):
+    n = start
+    i = 0
+
+    while n != 1:
+        if n in db.keys():
+            i += db[n]
+            break
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        i += 1
+
+    if count < i:
+        count = i
+        result = start
+    db[start] = i
+
+print(count, result)
