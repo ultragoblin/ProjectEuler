7d683e7c274e81fc3b9925a31b6c9bbb4c662a5f
ultragoblin <angry.person1@gmail.com>
2014-02-14 12:19:06 +0400

    p18 tabs

--- p18.py
+++ p18.py
@@ -1,6 +1,25 @@
 __author__ = 'guardian'
-
+result = 0
 file = open('p18_triangle', 'r')
-
+triangle = []
 for row in file:
-    print(row)
+	triangle.append([])
+	triangle[len(triangle) - 1] = row.strip().split(' ')
+	print(triangle[len(triangle) - 1])
+
+
+def max_sum(row, column, cur_sum):
+	global result
+	if row == len(triangle) - 1:
+		if cur_sum > result:
+			result = cur_sum
+		return
+	summ = cur_sum + int(triangle[row+1][column])
+	max_sum(row+1, column, summ)
+	summ = cur_sum + int(triangle[row+1][column+1])
+	max_sum(row+1, column+1, summ)
+
+max_sum(0, 0, int(triangle[0][0]))
+print(result)
+
+
